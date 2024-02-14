1b62155e0294e5efbcdac4a8f17afcdf2c93b40b
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-02-14 16:21:00 +0000

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"subscribers count"
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+    else:
+        data = response.json()
+        return data['data']['subscribers']
