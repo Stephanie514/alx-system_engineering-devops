3e69690944e68d0e8ad3fe6496515fd178301558
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-02-14 16:26:01 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code == 404:
+        return 0
+    elif response.status_code != 200:
         return 0
     else:
         data = response.json()
