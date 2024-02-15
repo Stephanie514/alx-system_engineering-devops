2163d4ce1b2153701a5ef2208317c3090891c5ad
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-02-15 08:46:23 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@
 import requests
 
 
-def def top_ten(subreddit):
+def def def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
